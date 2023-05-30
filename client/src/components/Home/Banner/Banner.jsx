3192d9d04ee91a6f75c82e7a74601029f8364dd2
Vullnet Rexhepi <vullnet.rexhepi657@gmail.com>
2023-05-30 19:01:39 +0200

    Second Commit

--- client/src/components/Home/Banner/Banner.jsx
+++ client/src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,23 @@
 import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png"
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner"> 
+      <div className="content">
+        <div className="text-content">
+            <h1>SALES</h1>
+            <p>
+                Convallis ineturm stri dis partuent
+                poseure mutani a quam montes kuli poseure
+                curae tempor
+            </p>
+            <div className="ctas">
+                <div className= "banner-cta">Read More</div>
+                <div className="banner-cta v2">Shop Now</div>
+            </div>
+        </div>
+        <img className="banner-img" src={BannerImg} alt="" />
+      </div>
+    </div>;
 };
 
 export default Banner;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,22 @@
 import "./Home.scss";
+import Category from "../Category/Category";
+import Products from "../Products/Products";
+import Banner from "./Banner/Banner";
+
 const Home = () => {
-    return <div className="home">Home</div>;
+    return(
+         <div >
+            <Banner />
+            <div className="main-content">
+                <div className="layout">
+                    <Category />
+                    <Products />
+                    </div>
+            </div>
+            
+        </div>
+    );
+             
 };
 
 export default Home;
